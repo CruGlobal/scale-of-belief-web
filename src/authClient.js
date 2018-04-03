045b1e73c2d632a5406504fd138b3af15a52008b
Bill Randall <william.randall@cru.org>
2018-04-03 09:50:03 -0400

    WIP

--- src/authClient.js
+++ src/authClient.js
@@ -5,33 +5,10 @@ import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'admin-on-rest'
 export default (type, params) => {
     switch (type) {
       case AUTH_LOGIN: {
-        const { username, password } = params
-        const authenticationUrl = 'https://thekey.me/cas'
-        const oauthPath = '/api/oauth/ticket'
-        let serviceParameter = '?service=' + encodeURIComponent(process.env.SERVICE_URL)
-
-        const request = new Request(authenticationUrl + oauthPath + serviceParameter, {
-          method: 'POST',
-          body: JSON.stringify({ username, password }),
-          headers: new Headers({
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + process.env.THE_KEY_ACCESS_TOKEN
-          })
-        })
-
-        return fetch(request).then(response => {
-          if (response.status !== 200) {
-            throw new Error(response.statusText)
-          }
-          return response.json()
-        }).then((token) => {
-          //TODO: Send token to the API to get back the JWT
-        })
-
         return Promise.resolve()
       }
       case AUTH_LOGOUT: {
-        break
+        return Promise.resolve()
       }
       case AUTH_ERROR: {
         const status  = params.message.status
@@ -41,7 +18,26 @@ export default (type, params) => {
         return Promise.resolve()
       }
       case AUTH_CHECK: {
-        break
+        const authenticationUrl = 'https://thekey.me/cas'
+        const oauthPath = '/api/oauth/ticket'
+        let serviceParameter = '?service=' + encodeURIComponent(process.env.SERVICE_URL)
+
+        const oauthRequest = new Request(authenticationUrl + oauthPath + serviceParameter, {
+          method: 'GET',
+          headers: new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + process.env.THE_KEY_ACCESS_TOKEN
+          })
+        })
+
+        return fetch(oauthRequest).then(response => {
+          if (response.status !== 200) {
+            throw new Error(response.statusText)
+          }
+          return response.json()
+        }).then((ticket) => {
+          //TODO: Send token to the API to get back the JWT
+        })
       }
       default: {
         return Promise.reject('Unknown method')
