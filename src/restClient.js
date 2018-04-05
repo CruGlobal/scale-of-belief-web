e21083da1a4937cf12ccc83868c31b50edb7b8c6
Bill Randall <william.randall@cru.org>
2018-04-05 13:22:12 -0400

    Handle 404 errors without console errors

--- src/restClient.js
+++ src/restClient.js
@@ -2,9 +2,13 @@ import {
     GET_ONE,
     GET_LIST,
     CREATE,
-    fetchUtils,
+    HttpError
 } from 'admin-on-rest'
 import { stringify } from 'query-string'
+const _extends2 = require('babel-runtime/helpers/extends');
+const _extends3 = _interopRequireDefault(_extends2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj } }
 
 const API_URL = process.env.REACT_APP_API_URL
 
@@ -48,6 +52,46 @@ const convertRESTRequestToHTTP = (type, resource, params) => {
   return { url, options }
 }
 
+// This is the same as the fetchUtils fetchJson except for how it handles 404 errors
+const fetchJson = (url, options) => {
+  var requestHeaders = options.headers || new Headers({
+    Accept: 'application/json'
+  })
+  if (!requestHeaders.has('Content-Type') && !(options && options.body && options.body instanceof FormData)) {
+    requestHeaders.set('Content-Type', 'application/json')
+  }
+  if (options.user && options.user.authenticated && options.user.token) {
+    requestHeaders.set('Authorization', options.user.token)
+  }
+
+  return fetch(url, (0, _extends3.default)({}, options, { headers: requestHeaders })).then(function (response) {
+    return response.text().then(function (text) {
+      return {
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers,
+        body: text
+      }
+    })
+  }).then(function (_ref) {
+      var status = _ref.status,
+          statusText = _ref.statusText,
+          headers = _ref.headers,
+          body = _ref.body
+
+      var json = void 0
+      try {
+        json = JSON.parse(body)
+      } catch (e) {
+        // not json, no big deal
+      }
+      if (status !== 404 && (status < 200 || status >= 300)) {
+        return Promise.reject(new HttpError.default(json && (json.message || statusText), status, json))
+      }
+      return { status: status, headers: headers, body: body, json: json }
+  })
+}
+
 /**
  * @param {Object} response HTTP response from fetch()
  * @param {String} type One of the constants appearing at the top if this file, e.g. 'UPDATE'
@@ -56,10 +100,15 @@ const convertRESTRequestToHTTP = (type, resource, params) => {
  * @returns {Object} REST response
  */
 const convertHTTPResponseToREST = (response, type, resource, params) => {
-  const { json } = response
-
   switch (type) {
-    case GET_LIST:
+    case GET_LIST: {
+      if (response.status && response.status === 404) {
+        return {
+          data: [],
+          total: 0
+        }
+      }
+      const { json } = response
       return {
         data: json.map((x) => {
           return {
@@ -69,7 +118,9 @@ const convertHTTPResponseToREST = (response, type, resource, params) => {
         }),
         total: json.length
       }
+    }
     default:
+      const { json } = response
       return { data: json }
   }
 }
@@ -81,7 +132,11 @@ const convertHTTPResponseToREST = (response, type, resource, params) => {
  * @returns {Promise} the Promise for a REST response
  */
 export default (type, resource, params) => {
-  const { fetchJson } = fetchUtils
   const { url, options } = convertRESTRequestToHTTP(type, resource, params)
-  return fetchJson(url, options).then(response => convertHTTPResponseToREST(response, type, resource, params))
+  return fetchJson(url, options)
+    .then(response => convertHTTPResponseToREST(response, type, resource, params), (error) => {
+      if (error.status === 404) {
+        convertHTTPResponseToREST(null, type, resource, params, error)
+      }
+    })
 }
