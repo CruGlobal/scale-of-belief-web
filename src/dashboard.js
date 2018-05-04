d1c2591ae85a1bd192e1e5c43189909f7cc16894
Bill Randall <william.randall@cru.org>
2018-05-04 13:36:42 -0400

    Add some styling

--- src/dashboard.js
+++ src/dashboard.js
@@ -18,23 +18,23 @@ export default () => (
     <Card>
       <ViewTitle title='People' />
       <CardText>
-        Content- Those with no desired relationship to faith. This includes people who are hostile
+        <span style={{color: '#f9b625'}}>Content</span> - Those with no desired relationship to faith. This includes people who are hostile
         towards Christianity, those who are content with their own religious beliefs that differ from Christianity,
         those who are content without religion, and those who have never heard the Gospel.
       </CardText>
 
       <CardText>
-        Curious- Those who are curious of spiritual things. This includes those who are specifically
+        <span style={{color: '#dd7d1b'}}>Curious</span> - Those who are curious of spiritual things. This includes those who are specifically
         interested in learning about Christianity, and those who are exploring religion in general.
       </CardText>
 
       <CardText>
-        Follower- Those who profess to be Christian. These people may or may not be actively looking for ways
+        <span style={{color: '#3eb1c8'}}>Follower</span> - Those who profess to be Christian. These people may or may not be actively looking for ways
         to grow spiritually. Either way, they are not yet committed to impacting the world around them for Christ.
       </CardText>
 
       <CardText>
-        Guide- Those advancing the mission of the Gospel. These are people mobilizing their faith through
+        <span style={{color: '#007398'}}>Guide</span> - Those advancing the mission of the Gospel. These are people mobilizing their faith through
         evangelism, discipleship, or serving others through personal or organizational ministry.
       </CardText>
     </Card>
