26f8b8185a2ff7e2c915b7fcd50d2b9de33281ef
Bill Randall <william.randall@cru.org>
2018-06-04 14:17:07 -0400

    Move the label colors to css

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardText } from 'material-ui/Card'
 import { ViewTitle } from 'admin-on-rest/lib/mui'
+import './App.css'
 
 export default () => (
   <div>
@@ -18,23 +19,23 @@ export default () => (
     <Card>
       <ViewTitle title='People' />
       <CardText>
-        <span style={{color: '#f9b625'}}>Content</span> - Those with no desired relationship to faith. This includes people who are hostile
+        <span className='content-label'>Content</span> - Those with no desired relationship to faith. This includes people who are hostile
         towards Christianity, those who are content with their own religious beliefs that differ from Christianity,
         those who are content without religion, and those who have never heard the Gospel.
       </CardText>
 
       <CardText>
-        <span style={{color: '#dd7d1b'}}>Curious</span> - Those who are curious of spiritual things. This includes those who are specifically
+        <span className='curious-label'>Curious</span> - Those who are curious of spiritual things. This includes those who are specifically
         interested in learning about Christianity, and those who are exploring religion in general.
       </CardText>
 
       <CardText>
-        <span style={{color: '#3eb1c8'}}>Follower</span> - Those who profess to be Christian. These people may or may not be actively looking for ways
+        <span className='follower-label'>Follower</span> - Those who profess to be Christian. These people may or may not be actively looking for ways
         to grow spiritually. Either way, they are not yet committed to impacting the world around them for Christ.
       </CardText>
 
       <CardText>
-        <span style={{color: '#007398'}}>Guide</span> - Those advancing the mission of the Gospel. These are people mobilizing their faith through
+        <span className='guide-label'>Guide</span> - Those advancing the mission of the Gospel. These are people mobilizing their faith through
         evangelism, discipleship, or serving others through personal or organizational ministry.
       </CardText>
     </Card>
