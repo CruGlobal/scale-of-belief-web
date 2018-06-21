88e0683d3a4bcbc7524e72bfb9623a68804508fe
Bill Randall <william.randall@cru.org>
2018-06-21 14:11:57 -0400

    Enable specific classes to be set on TooltipTextInput

--- src/TooltipTextInput.js
+++ src/TooltipTextInput.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import TextField from 'material-ui/TextField'
 
-const TooltipTextInput = ({ input, label, meta: { touched, error }, ...custom }) => (
+const TooltipTextInput = ({ input, label, meta: { touched, error }, className, ...custom }) => (
   <div style={{width: '256px'}} data-tip>
     <TextField
       hintText={label}
       floatingLabelText={label}
       errorText={touched && error}
+      className={className}
       {...input} />
   </div>
 )
